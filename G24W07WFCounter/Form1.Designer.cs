c8b802c916552165389d640e15e9832111923e08
BWOH <bwoh@outlook.kr>
2024-10-14 02:11:53 +0900

    초기 커밋.

---
+++ G24W07WFCounter/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace G24W07WFCounter {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            labelCount = new Label();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // labelCount
+            // 
+            labelCount.BackColor = SystemColors.Info;
+            labelCount.Font = new Font("맑은 고딕", 36F);
+            labelCount.Location = new Point(-1, 9);
+            labelCount.Name = "labelCount";
+            labelCount.Size = new Size(291, 251);
+            labelCount.TabIndex = 0;
+            labelCount.Text = "0";
+            labelCount.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(-1, 280);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(291, 51);
+            btnAdd.TabIndex = 1;
+            btnAdd.Text = "증가";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += OnAdd;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(290, 332);
+            Controls.Add(btnAdd);
+            Controls.Add(labelCount);
+            Name = "Form1";
+            Text = "카운터";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label labelCount;
+        private Button btnAdd;
+    }
+}
---
+++ G24W07WFCounter/Form1.cs
@@ -0,0 +1,15 @@
+namespace G24W07WFCounter {
+    public partial class Form1 : Form {
+        private int Count = 0;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void OnAdd(object sender, EventArgs e) {
+            //labelCount.Text = (++Count).ToString();
+            //labelCount.Text = "" + ++Count;
+            labelCount.Text = $"{++Count}";
+        }
+    }
+}
